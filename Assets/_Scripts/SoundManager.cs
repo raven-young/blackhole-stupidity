5afd5e19d8237f0de193455622ed6a55d860f279
pdegen <peter.methys@gmail.com>
2023-07-27 17:42:05 +0200

    game params excluded from gitignore

--- Assets/_Scripts/SoundManager.cs
+++ Assets/_Scripts/SoundManager.cs
@@ -48,4 +48,16 @@ public class SoundManager : MonoBehaviour
     {
         AudioListener.volume = volume;
     }
+
+    //public IEnumerator ChangeMusic(AudioClip clip, float fadeoutDuration = 0f, float fadeinDuration = 0f, float offset = 0f)
+    //{
+    //    _musicSource.DOFade(0, fadeoutDuration);
+    //    //DOTween.To(() => _musicSource.volume, x => _musicSource.volume = x, 0, fadeoutDuration);
+    //    yield return new WaitForSeconds(fadeoutDuration);
+    //    yield return new WaitForSeconds(offset);
+    //    _musicSource.Stop();
+    //    _musicSource.PlayOneShot(clip);
+    //    _musicSource.DOFade(1, fadeinDuration);
+    //    yield return new WaitForSeconds(fadeinDuration);
+    //}
 }
