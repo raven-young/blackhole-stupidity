5a851a0048c7483cb8f712f9d8bb23f9a9c56d8e
pdegen <peter.methys@gmail.com>
2023-07-28 22:48:02 +0200

    improved audio transitions

--- Assets/_Scripts/SoundManager.cs
+++ Assets/_Scripts/SoundManager.cs
@@ -6,7 +6,7 @@ using DG.Tweening;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
-    [SerializeField] private AudioSource _musicSource, _musicSource2, _effectsSource;
+    [SerializeField] private AudioSource _musicSource1, _musicSource2, _effectsSource;
     [SerializeField] private AudioClip _backgroundMusic, _nervousMusic, _mainMenuMusic, _dialogueMusic;
 
     private AudioClip _activeClip;
@@ -27,7 +27,9 @@ public class SoundManager : MonoBehaviour
 
     private void Start()
     {
-        PlayMusic(_mainMenuMusic);
+        _musicSource1.PlayOneShot(_mainMenuMusic);
+        _musicSource2.PlayOneShot(_dialogueMusic);
+        _musicSource2.DOFade(0, 0);
     }
 
     private void OnEnable()
@@ -42,18 +44,22 @@ public class SoundManager : MonoBehaviour
         GameManager.OnExitedDangerZone -= SwapBG;
     }
 
-    public void ChangeToBG()
+    public IEnumerator ChangeToBG()
     {
-        _musicSource.Stop();
-        _musicSource.PlayOneShot(_backgroundMusic);
+        _musicSource2.DOFade(0, 3f);
+        yield return new WaitForSeconds(3f);
+        _musicSource1.PlayOneShot(_backgroundMusic);
         _musicSource2.PlayOneShot(_nervousMusic);
+        _musicSource1.DOFade(1, 3f);
         _musicSource2.DOFade(0, 0);
     }
 
     public void ChangeToDialogueTheme()
     {
-        _musicSource.Stop();
-        _musicSource.PlayOneShot(_dialogueMusic);
+        //_musicSource.Stop();
+        //_musicSource.PlayOneShot(_dialogueMusic);
+        _musicSource1.DOFade(0, 3f);
+        _musicSource2.DOFade(1, 3f);
     }
 
     public void PlaySound(AudioClip clip, float volumeScale = 1f)
@@ -63,7 +69,7 @@ public class SoundManager : MonoBehaviour
 
     public void PlayMusic(AudioClip clip, float volumeScale = 1f)
     {
-        _musicSource.PlayOneShot(clip, volumeScale);
+        _musicSource1.PlayOneShot(clip, volumeScale);
     }
     public void ChangeSFXVolume(float volume)
     {
@@ -71,13 +77,13 @@ public class SoundManager : MonoBehaviour
     }
     public void ChangeMusicVolume(float volume, float fadeDuration = 0f)
     {
-        _musicSource.DOFade(volume, fadeDuration);
+        _musicSource1.DOFade(volume, fadeDuration);
         _musicSource2.DOFade(volume, fadeDuration);
     }
 
     public void ChangeActiveMusicVolume(float volume, float fadeDuration = 0f)
     {
-        var source = _activeClip == _nervousMusic ? _musicSource2 : _musicSource;
+        var source = _activeClip == _nervousMusic ? _musicSource2 : _musicSource1;
         source.DOFade(volume, fadeDuration);
     }
 
@@ -92,13 +98,13 @@ public class SoundManager : MonoBehaviour
 
         if (GameManager.Instance.InDangerZone)
         {
-            _musicSource.DOFade(0, fadeDuration);
+            _musicSource1.DOFade(0, fadeDuration);
             _musicSource2.DOFade(1, fadeDuration);
             _activeClip = _nervousMusic;
         }
         else
         {
-            _musicSource.DOFade(1, fadeDuration);
+            _musicSource1.DOFade(1, fadeDuration);
             _musicSource2.DOFade(0, fadeDuration);
             _activeClip = _backgroundMusic;
         }
@@ -106,13 +112,13 @@ public class SoundManager : MonoBehaviour
 
     public IEnumerator ChangeMusic(AudioClip clip, float fadeoutDuration = 0f, float fadeinDuration = 0f, float offset = 0f)
     {
-        _musicSource.DOFade(0, fadeoutDuration);
+        _musicSource1.DOFade(0, fadeoutDuration);
         //DOTween.To(() => _musicSource.volume, x => _musicSource.volume = x, 0, fadeoutDuration);
         yield return new WaitForSeconds(fadeoutDuration);
         yield return new WaitForSeconds(offset);
-        _musicSource.Stop();
-        _musicSource.PlayOneShot(clip);
-        _musicSource.DOFade(1, fadeinDuration);
+        _musicSource1.Stop();
+        _musicSource1.PlayOneShot(clip);
+        _musicSource1.DOFade(1, fadeinDuration);
         yield return new WaitForSeconds(fadeinDuration);
     }
 }
