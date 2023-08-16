ee2df340c3672c379f2c44edd8027cd0b60408e6
pdegen <peter.methys@gmail.com>
2023-08-16 16:28:43 +0200

    tweened menu buttons scale

--- Assets/_Scripts/Button.cs
+++ Assets/_Scripts/Button.cs
@@ -6,10 +6,12 @@ public class Button : MonoBehaviour
 {
     [SerializeField] private AudioClip _selectedClip, _pressedClip, _failedClip;
     private AudioSource _audioSource;
+    private Vector3 _baseScale;
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
+        _baseScale = gameObject.transform.localScale;
     }
 
     public void PlayPressed(bool success = true)
@@ -23,5 +25,15 @@ public class Button : MonoBehaviour
     public void PlaySelected()
     {
         _audioSource.PlayOneShot(_selectedClip);
+    }
+
+    public void IncreaseScale()
+    {
+        gameObject.transform.localScale = 1.1f * _baseScale;
+    }
+
+    public void DecreaseScale()
+    {
+        gameObject.transform.localScale = _baseScale / 1.1f;
     }
 }
